b65dea14e32be1012b6e41ce0a37d58bb363636a
RC13967 <66828308+RC13967@users.noreply.github.com>
2021-07-28 16:30:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//creates page number buttons
 function pagination (startidx){
   const pagediv =  document.createElement("div");
   pagediv.className = "page-div";
@@ -5,57 +6,83 @@ function pagination (startidx){
   firstpage.innerHTML = "first";
   pagediv.append(firstpage);
   firstpage.onclick = function(){
-     document.querySelector(".page-div").remove();
-       document.querySelector(".sport-list").remove()
-    pagination(0);
-    getsports();
+     document.querySelector(".page-div").remove();  //removes existing page number buttons
+       document.querySelector(".sport-list").remove() //removes existing data in the page
+    pagination(0); 
+    getsports(); //loads data of first page
   }
+  //creating button numbers
   for (let i = +startidx;i< +startidx +10;i++){
     const pagebutton = document.createElement("button");
     pagebutton.className= "page-button"
     pagebutton.innerHTML = i;
    if(i==startidx){
-     pagebutton.setAttribute("class","clickedpagebutton");
+     pagebutton.setAttribute("class","clickedpagebutton"); /*making the first button of every page actives
+     (this loads data of first button in each page)*/
    }
     pagediv.append(pagebutton);
     pagebutton.onclick = function (){
-       document.querySelector(".clickedpagebutton").removeAttribute("class");
+      //removes class for previously clicked button
+       document.querySelector(".clickedpagebutton").removeAttribute("class"); 
+       //assigns class for newly clicked button(this changes button color whenever it is clicked)
       pagebutton.setAttribute("class","clickedpagebutton");
-      document.querySelector(".sport-list").remove();
+      document.querySelector(".sport-list").remove(); //removes existing data in the page
       localStorage.setItem("clickedpage",i)
-      getsports();
+      getsports(); //gets data from api
     }
     
   }
   const nextbutton = document.createElement("button");
-  nextbutton.innerHTML = `${+startidx + 10}-${+startidx + 19}`;
+  //creates label for the button as numbers ranging from first button to last button
+  nextbutton.innerHTML = `${+startidx + 10}-${+startidx + 19}`; 
   pagediv.append(nextbutton);
   nextbutton.onclick = function () {
-    localStorage.setItem("currentindex",startidx);
-    nextitems();
-    
+    localStorage.setItem("currentindex",startidx); //stores the number of first button in local storage()
+    nextitems();   //changes button numbers(increases by 10)
   }
- 
-  
  document.body.append(pagediv);
  }
 
  function nextitems(){
-  var currentindex = localStorage.getItem("currentindex");
-    document.querySelector(".page-div").remove();
-      document.querySelector(".sport-list").remove();
-  pagination(+currentindex + 10);
-    getsports();
-  
-
+  var currentindex = localStorage.getItem("currentindex");  
+    document.querySelector(".page-div").remove();  //removes existing buttons
+      document.querySelector(".sport-list").remove(); //removes existing data in the page
+  pagination(+currentindex + 10); //creating new buttons with numbers increased by 10
+    getsports(); //gets new data from api
 }
+
+ //gets the data from api
+async function getsports() {      
+  const data = await fetch(
+    "https://www.scorebat.com/video-api/v1/",
+    {
+      method: "GET"
+    }
+  );
+  try {
+    const sportjson = await data.json();
+    let arr =[];
+    startidx = localStorage.getItem("clickedpage");  //gets the index of first button from local storage
+    // sets the first button value to zero if it is run for the first time(else, data cant be loaded without start index)
+    if(startidx ==null){startidx = 0}  
+    for(let i=startidx;i< +startidx + 10;i++){
+        arr.push(sportjson[i]) ;      //stores the first 10 data values in an array(with the help of start index)
+ }
+ loadsports(arr);  //loads data in the page
+
+  }
+
+  catch{
+    alert("check your internet");  //alerts the user if fetch fails
+  }
   
-  
-            
+}
+//loading the data in the page         
   function loadsports(sports) {
       const sportList = document.createElement("div");
       sportList.className = "sport-list";
       sports.forEach((sport) => {
+        //loads the required data for each match
         const sportContainer = document.createElement("div");
         sportContainer.className = "sport-container";
         sportContainer.innerHTML = `
@@ -75,34 +102,6 @@ function pagination (startidx){
     
       document.body.append(sportList);
     }
-    
-    
-    
-    async function getsports() {
-      
-      
-        
-      const data = await fetch(
-        "https://www.scorebat.com/video-api/v1/",
-        {
-          method: "GET"
-        }
-      );
-      try {
-        const sportjson = await data.json();
-        let arr =[];
-        startidx = localStorage.getItem("clickedpage");
-        if(startidx ==null){startidx = 0}
-        for(let i=startidx;i< +startidx + 10;i++){
-            arr.push(sportjson[i])
-     }
-     loadsports(arr);
 
-      }
-
-      catch{
-        alert("check your internet");
-      }
-      
-    }
+ 
   
